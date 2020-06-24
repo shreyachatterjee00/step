c9fa3bcf9c2423ac182d16526ffe8426f96f5e87
Shreya Chatterjee <schatterjee22@ucla.edu>
2020-06-24 17:50:14 +0000

    Format / indentation / general design fixes in response to original pull request review.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,52 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*
-Shows about text when the "About" button is pressed.
-*/
+/* Shows about text when the "About" button is pressed. */
 function showAbout () {
-    aboutText = "I'm a rising junior at UCLA, majoring in Linguistics and Computer Science (that's one major)! In my free time, I enjoy reading and playing the piano. \n\n Recently, I've started learning to cook (I'm going to need this for my college apartment next year) and playing Stardew Valley (turns out video games are a great way to stay connected to friends during this time)!";
-    var aboutContainer = document.getElementById('aboutSpace');
-    //if about pressed, either remove / show about text depending on what is already there
+    const aboutText = "I'm a rising junior at UCLA, majoring in Linguistics and Computer Science (that's one major)! In my free time, I enjoy reading and playing the piano. \n\n Recently, I've started learning to cook (I'm going to need this for my college apartment next year) and playing Stardew Valley (turns out video games are a great way to stay connected to friends during this time)!";
+    const aboutContainer = document.getElementById('aboutSpace');
+    //Change the text in the aboutSpace element 
     if (aboutContainer.innerText === "") {
-        //change inner text to about text, and div class to CSS about class
+        //Change inner text to about text, and div class to CSS about class
         aboutContainer.innerText = aboutText;
-        aboutContainer.className="background";
+        aboutContainer.className = "background";
     }
     else {
         aboutContainer.innerText = "";
-        aboutContainer.className="";
+        aboutContainer.className= "";
     }
 }
 
-/*
-Shows pictures when the "Pictures" button is pressed.
-*/
+/* Shows pictures when the "Pictures" button is pressed. */
 function showPics() {
-    //array of images
-    let imgs = ["ucla1.JPG", "ucla2.jpg.JPG", "book.JPG", "harrypotter.JPG"]
-    //random int from 0 - 4
-    var randInt = (Math.floor(Math.random() * 5));
-    var randImg = imgs[randInt];
+    const imgs = ["ucla1.JPG", "ucla2.jpg.JPG", "book.JPG", "harrypotter.JPG"]
+    const srcBase = "https://8080-0ba309ed-2f3c-4b55-83b6-7741ff94bec3.us-west1.cloudshell.dev/base"
+    const picPath = "https://8080-0ba309ed-2f3c-4b55-83b6-7741ff94bec3.us-west1.cloudshell.dev/images/"
     
+    //Random int from 0 - size of images array
+    const randInt = (Math.floor(Math.random() * imgs.length));
+    const randImg = imgs[randInt];
+    
+    const div = document.getElementById('picSpace');
+    const picContainer = div.getElementsByTagName('img')[0];
 
-
-    var img1 = "images/ucla1.JPG";
-    var div = document.getElementById('picSpace');
-    var picContainer = document.getElementById('picSpace').getElementsByTagName('img')[0];
-
-    //if pictures pressed, either remove / show picture depending on what is already there
-    if (picContainer.src==="https://8080-0ba309ed-2f3c-4b55-83b6-7741ff94bec3.us-west1.cloudshell.dev/base") {
-        picContainer.src = "https://8080-0ba309ed-2f3c-4b55-83b6-7741ff94bec3.us-west1.cloudshell.dev/images/" + randImg;
-        picContainer.style.display = 'block';
+    //If pictures pressed, either remove / show picture depending on what is already there
+    if (picContainer.src === srcBase) {
+        picContainer.src = picPath + randImg;
+        picContainer.className = "displayImage";
         div.className = 'background';
-        
         
     }
     else {
-        picContainer.src="https://8080-0ba309ed-2f3c-4b55-83b6-7741ff94bec3.us-west1.cloudshell.dev/base";
-        picContainer.style.display = 'none';
-        div.className ="";
+        picContainer.src = srcBase;
+        picContainer.className = "image";
+        div.className = "";
     }
     
 }
